847f718398047efb31c859cfa92d306c26f4e5a2
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-20 12:50:00 +0200

    three test cases run, all passed

--- minmaxdiff/test_min_max_diff.py
+++ minmaxdiff/test_min_max_diff.py
@@ -6,5 +6,13 @@ class TestMinMaxDiff(unittest.TestCase):
         numbers = [1, 2, 3, 4, 5]
         self.assertEqual(min_max_diff(numbers), 4)
 
+    def test_min_max_diff_empty_list(self):
+        numbers = []
+        self.assertEqual(min_max_diff(numbers), None)
+
+    def test_min_max_diff_zero(self):
+        numbers = [0]
+        self.assertEqual(min_max_diff(numbers), 0)
+
 if __name__ == '__main__':
     unittest.main()
