c33bf0468603ebac15163fd2826e6a504e25edde
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-20 12:24:14 +0200

    function can find the highest and the lowest value in a list

--- minmaxdiff/min_max_diff.py
+++ minmaxdiff/min_max_diff.py
@@ -1,3 +1,26 @@
 # Create a function called `min_max_diff` that takes a list of numbers as parameter
 # and returns the difference between maximum and minimum values in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+def min_max_diff(list_of_numbers):
+    the_max = list_of_numbers[0]
+    for num in list_of_numbers:
+        if the_max < num:
+            the_max = num
+    print(the_max)
+    the_min = list_of_numbers[0]
+    for num in list_of_numbers:
+        if the_min > num:
+            the_min = num
+    print(the_min)
+
+
+list_to_examine = [2, 3, 8]
+
+print(min_max_diff(list_to_examine))
+
+"""
+max(list_of_numbers) = highest_value
+min(list_of_numbers) = lowest_value
+difference = highest_value - lowest_value
+return difference"""
