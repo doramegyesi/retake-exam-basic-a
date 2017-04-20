690a8541b5f64b102053a104251fa587fecbba02
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-20 13:17:43 +0200

    class is represented as required

--- library/library.py
+++ library/library.py
@@ -7,20 +7,33 @@
 # We should be able to query the favourite author (who has written the most books in the shelf)
 # We should be able to query the earliest published books.
 # We should be able to query the latest published books.
-# Bookself should have a method whitch give us information about the number of books, the earliest and the latest released books, and the favourite author 
-   
-my_shelf = BookShelf()
-print(my_shelf.books())
+# Bookself should have a method whitch give us information about the number of books,
+#the earliest and the latest released books, and the favourite author
+
+class Book():
+    def __init__(self, author, title, release_year):
+        self.title = title
+        self.author = author
+        self.release_year = release_year
+
+    def __repr__(self):
+        return str(self.author) + ": " + str(self.title) + " (" + self.release_year + ")"
+
+
+book = Book("Sylvia Plath", "The Bell Jar", "1963")
+print(book)
+#my_shelf = BookShelf()
+#print(my_shelf.books())
 # Should print out:
 # You have no books here.
-    
-my_shelf.put("Douglas Adams", "The Hitchhiker's Guide to the Galaxy", 1979)
-my_shelf.put("Douglas Adams", "Mostly Harmless", 1992)
-my_shelf.put("Frank Herbert", "Dune", 1965)
-my_shelf.put("Frank Herbert", "The Dragon in the Sea", 1957)
-my_shelf.remove("The Dragon in the Sea")
 
-print(my_shelf.books())
+#my_shelf.put("Douglas Adams", "The Hitchhiker's Guide to the Galaxy", 1979)
+#my_shelf.put("Douglas Adams", "Mostly Harmless", 1992)
+#my_shelf.put("Frank Herbert", "Dune", 1965)
+#my_shelf.put("Frank Herbert", "The Dragon in the Sea", 1957)
+#my_shelf.remove("The Dragon in the Sea")
+
+#print(my_shelf.books())
 # Should print out:
 # You have 3 books.
 # Earliest released: Frank Herbert : Dune (1965)
