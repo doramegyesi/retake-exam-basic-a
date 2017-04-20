2a7e8f529a0d608e3ba213530ba0be411f72a3da
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-20 12:28:13 +0200

    function works ad required

--- minmaxdiff/min_max_diff.py
+++ minmaxdiff/min_max_diff.py
@@ -7,20 +7,16 @@ def min_max_diff(list_of_numbers):
     for num in list_of_numbers:
         if the_max < num:
             the_max = num
-    print(the_max)
     the_min = list_of_numbers[0]
     for num in list_of_numbers:
         if the_min > num:
             the_min = num
-    print(the_min)
+    difference = the_max - the_min
+    return difference
 
 
 list_to_examine = [2, 3, 8]
+another_list = [5, 21, 68, 71, 105, 33]
 
 print(min_max_diff(list_to_examine))
-
-"""
-max(list_of_numbers) = highest_value
-min(list_of_numbers) = lowest_value
-difference = highest_value - lowest_value
-return difference"""
+print(min_max_diff(another_list))
